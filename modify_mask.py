Masking changes and other fixes

modify_mask.py
@@ -1,11 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from astropy.io import fits
 from astropy.table import Table
 from astropy.wcs import WCS
 import numpy as np
 
-def modify_mask(infile,outfile,table,radius,fluxlim,save_filtered=None):
+def ellipse(x,y,xp,yp,major,minor,angle):
+    #tests if a point[xp,yp] is within
+    #boundaries defined by the ellipse
+    #of center[x,y], diameter d D, and tilted at angle
+
+    cosa=np.cos(angle*np.pi/180)
+    sina=np.sin(angle*np.pi/180)
+    dd=(minor/2.0)**2.0
+    DD=(major/2.0)**2.0
+
+    a=np.power(cosa*(xp-x)+sina*(yp-y),2)
+    b=np.power(sina*(xp-x)-cosa*(yp-y),2)
+    return (a/dd)+(b/DD)
+
+def modify_mask(infile,outfile,table,radius,fluxlim,save_filtered=None,do_extended=False,cellsize=1.5,pointsize=30.0):
     """Take a pre-existing mask file, in infile: find all entries in FITS
     table table that lie in the map region, and add their positions to
     the mask with a fixed radius radius in pixels: write the mask out
@@ -27,26 +41,42 @@ def modify_mask(infile,outfile,table,radius,fluxlim,save_filtered=None):
     _,_,ymax,xmax=map.shape
     x,y,_,_=w.wcs_world2pix(t['RA'],t['DEC'],0,0,0)
     filter=(x>=0) & (x<xmax) & (y>=0) & (y<ymax)
+    t=t[filter]
     x=x[filter]
     y=y[filter]
-    for xv,yv in zip(x,y):
-        cxmin=xv-radius-1
+    for i,(xv,yv) in enumerate(zip(x,y)):
+        do_ellipse=False
+        r=radius
+        if do_extended:
+            # check whether the major axis exceeds a limit
+            major=t[i]['Maj']
+            minor=t[i]['Min']
+            pa=t[i]['PA']
+            if major>pointsize:
+                do_ellipse=True
+                r=major+radius
+                # print 'Doing ellipse with',major,minor,pa
+
+        cxmin=xv-r-1
         if cxmin<0: cxmin=0
-        cxmax=xv+radius+1
+        cxmax=xv+r+1
         if cxmax>xmax: cxmax=xmax
-        cymin=yv-radius-1
+        cymin=yv-r-1
         if cymin<0: cymin=0
-        cymax=yv+radius+1
+        cymax=yv+r+1
         if cymax>ymax: cymax=ymax
         X, Y = np.meshgrid(np.arange(cxmin,cxmax,1.0), np.arange(cymin,cymax,1.0))
-        rv=np.sqrt((X+0.5-xv)**2.0+(Y+0.5-yv)**2.0)
-        mask[0,0,Y[rv<radius].astype(int),X[rv<radius].astype(int)]=1
+        if do_ellipse:
+            ellv=ellipse(xv,yv,X,Y,major/cellsize+radius*2.0,minor/cellsize+radius*2.0,pa)
+            mask[0,0,Y[ellv<1.0].astype(int),X[ellv<1.0].astype(int)]=1
+        else:
+            rv=np.sqrt((X+0.5-xv)**2.0+(Y+0.5-yv)**2.0)
+            mask[0,0,Y[rv<radius].astype(int),X[rv<radius].astype(int)]=1
 
     hdu[0].data=(map.astype(int) | mask).astype(np.float32)
     hdu.writeto(outfile,clobber=True)
     if save_filtered is not None:
-        newt=t[filter]
-        newt.write(save_filtered,overwrite=True)
+        t.write(save_filtered,overwrite=True)
         
 
 if __name__=='__main__':
@@ -55,9 +85,9 @@ if __name__=='__main__':
     infile=sys.argv[1]
     outfile=sys.argv[2]
     table=sys.argv[3]
-    radius=int(sys.argv[4])
+    radius=float(sys.argv[4])
     try:
         sf=sys.argv[5]
     except:
         sf=None
-    modify_mask(infile,outfile,table,radius,500,save_filtered=sf)
+    modify_mask(infile,outfile,table,radius,500,save_filtered=sf,do_extended=True)