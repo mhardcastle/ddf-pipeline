48e6244c76823fc64e8a36af148ce8faf21ffd21
Wendy L. Williams <wndywllms@gmail.com>
2017-02-03 18:02:49 +0000

    bootstrap each observation separately

--- bootstrap.py
+++ bootstrap.py
@@ -49,136 +49,148 @@ def run_bootstrap(o):
 
     # We use the individual ms in mslist.
     mslist=[s.strip() for s in open(o['mslist']).readlines()]
-
-    # Get the frequencies -- need to take this from the MSs
-
-    freqs=[]
-    for ms in mslist:
-        t = pt.table(ms+'/SPECTRAL_WINDOW', readonly=True, ack=False)
-        freqs.append(t[0]['REF_FREQUENCY'])
-
-    # sort to work in frequency order
-
-    freqs,mslist = (list(x) for x in zip(*sorted(zip(freqs, mslist), key=lambda pair: pair[0])))
-
-    for f,m in zip(freqs,mslist):
-        print m,f
-
-    # First we need to do a MSMF clean to make an initial mask; then
-    # we can use this for each of the bands. We use the
-    # lowest-frequency dataset.
-
-    ddf_image('image_low_initial_MSMF',mslist[0],cleanmode='MSMF',ddsols='killms_p1',applysols='P',threshold=5e-3,majorcycles=3,robust=low_robust,uvrange=low_uvrange,beamsize=20,imsize=o['bsimsize'],cellsize=o['bscell'],options=o,colname=o['colname'])
-    make_mask('image_low_initial_MSMF.app.restored.fits',20,options=o)
-
-    # now loop over the MSs to make the images
-    for i,ms in enumerate(mslist):
-        imroot='image_low_%i_SSD' % i
-        ddf_image(imroot,ms,cleanmask='image_low_initial_MSMF.app.restored.fits.mask.fits',cleanmode='SSD',ddsols='killms_p1',applysols='P',majorcycles=3,robust=low_robust,uvrange=low_uvrange,beamsize=20,imsize=o['bsimsize'],cellsize=o['bscell'],options=o,colname=o['colname'])
-        make_mask(imroot+'.app.restored.fits',15,options=o)
-        ddf_image(imroot+'m',ms,cleanmask=imroot+'.app.restored.fits.mask.fits',previous_image=imroot,reuse_psf=True,use_dicomodel=True,majorcycles=2,cleanmode='SSD',ddsols='killms_p1',applysols='P',robust=low_robust,uvrange=low_uvrange,beamsize=20,saveimages='H',imsize=o['bsimsize'],cellsize=o['bscell'],dirty_from_resid=True,options=o,colname=o['colname'])
-
-    from make_cube import make_cube
-
-    #make the cube
-    if os.path.isfile('cube.fits'):
-        warn('Cube file exists, skipping cube assembly')
-    else:
-        warn('Making the cube')
-        make_cube('cube.fits',['image_low_%i_SSDm.int.restored.fits' % i for i in range(len(mslist))],freqs)
-    if os.path.isfile('cube.pybdsm.srl'):
-        warn('Source list exists, skipping source extraction')
-    else:
-        warn('Running PyBDSM, please wait...')
-        img=bdsm.process_image('cube.fits',thresh_pix=5,rms_map=True,atrous_do=True,atrous_jmax=2,group_by_isl=True,rms_box=(80,20), adaptive_rms_box=True, adaptive_thresh=80, rms_box_bright=(35,7),mean_map='zero',spectralindex_do=True,specind_maxchan=1,debug=True,kappa_clip=3,flagchan_rms=False,flagchan_snr=False,incl_chan=True,spline_rank=1)
-        # Write out in ASCII to work round bug in pybdsm
-        img.write_catalog(catalog_type='srl',format='ascii',incl_chan='true')
-        img.export_image(img_type='rms',img_format='fits')
-
-    from make_fitting_product import make_catalogue
-    import fitting_factors
-    import find_outliers
-
-    # generate the fitting product
-    if os.path.isfile('crossmatch-1.fits'):
-        warn('Crossmatch table exists, skipping crossmatch')
-    else:
- 
-        t = pt.table(mslist[0]+ '/FIELD', readonly=True, ack=False)
-        direction = t[0]['PHASE_DIR']
-        ra, dec = direction[0]
-
-        if (ra<0):
-            ra+=2*np.pi
-        ra*=180.0/np.pi
-        dec*=180.0/np.pi
-
-        cats=zip(o['catalogues'],o['names'],o['groups'],o['radii'])
-        make_catalogue('cube.pybdsm.srl',ra,dec,2.5,cats)
     
-    freqlist=open('frequencies.txt','w')
-    for n,f in zip(o['names'],o['frequencies']):
-        freqlist.write('%f %s_Total_flux %s_E_Total_flux False\n' % (f,n,n))
-    for i,f in enumerate(freqs):
-        freqlist.write('%f Total_flux_ch%i E_Total_flux_ch%i True\n' % (f,i+1,i+1))
-    freqlist.close()
-
-    # Now call the fitting code
-
-    if os.path.isfile('crossmatch-results-1.npy'):
-        warn('Results 1 exists, skipping first fit')
-    else:
-        if o['use_mpi']:
-            run('mpiexec -np 24 fitting_factors.py 1',dryrun=o['dryrun'],log=None,quiet=o['quiet'])
-        else:
-            fitting_factors.run_all(1)
-
-    if os.path.isfile('crossmatch-2.fits'):
-        warn('Second crossmatch exists, skipping outlier rejection')
-    else:
-        find_outliers.run_all(1)
+    obsids = [ms.split('_')[0] for ms in mslist]
+    Uobsid = np.unique(np.array(obsids))
     
-    if os.path.isfile('crossmatch-results-2.npy'):
-        warn('Results 1 exists, skipping second fit')
-    else:
-        if o['use_mpi']:
-            run('mpiexec -np 24 fitting_factors.py 2',dryrun=o['dryrun'],log=None,quiet=o['quiet'])
+    for obsid in obsids:
+        
+        warn('Running bootstrap for obsid %s' % obsid)
+
+        # Get the frequencies -- need to take this from the MSs
+        
+        omslist = [ms for ms in mslist if obsid in ms]
+
+        freqs=[]
+        for ms in omslist:
+            t = pt.table(ms+'/SPECTRAL_WINDOW', readonly=True, ack=False)
+            freqs.append(t[0]['REF_FREQUENCY'])
+
+        # sort to work in frequency order
+
+        freqs,omslist = (list(x) for x in zip(*sorted(zip(freqs, omslist), key=lambda pair: pair[0])))
+
+        for f,m in zip(freqs,omslist):
+            print m,f
+
+        # First we need to do a MSMF clean to make an initial mask; then
+        # we can use this for each of the bands. We use the
+        # lowest-frequency dataset.
+
+        ddf_image('image_%s_low_initial_MSMF'%obsid,omslist[0],cleanmode='MSMF',ddsols='killms_p1',applysols='P',threshold=5e-3,majorcycles=3,robust=low_robust,uvrange=low_uvrange,beamsize=20,imsize=o['bsimsize'],cellsize=o['bscell'],options=o,colname=o['colname'])
+        make_mask('image_%s_low_initial_MSMF.app.restored.fits'%obsid,20,options=o)
+
+        # now loop over the MSs to make the images
+        for i,ms in enumerate(omslist):
+            imroot='image_%s_low_%i_SSD'%(obsid,i)
+            ddf_image(imroot,ms,cleanmask='image_low_initial_MSMF.app.restored.fits.mask.fits',cleanmode='SSD',ddsols='killms_p1',applysols='P',majorcycles=3,robust=low_robust,uvrange=low_uvrange,beamsize=20,imsize=o['bsimsize'],cellsize=o['bscell'],options=o,colname=o['colname'])
+            make_mask(imroot+'.app.restored.fits',15,options=o)
+            ddf_image(imroot+'m',ms,cleanmask=imroot+'.app.restored.fits.mask.fits',previous_image=imroot,reuse_psf=True,use_dicomodel=True,majorcycles=2,cleanmode='SSD',ddsols='killms_p1',applysols='P',robust=low_robust,uvrange=low_uvrange,beamsize=20,saveimages='H',imsize=o['bsimsize'],cellsize=o['bscell'],dirty_from_resid=True,options=o,colname=o['colname'])
+
+        from make_cube import make_cube
+
+        #make the cube
+        if os.path.isfile('%scube.fits'%obsid):
+            warn('Cube file exists, skipping cube assembly')
         else:
-            fitting_factors.run_all(2)
+            warn('Making the cube')
+            make_cube('%scube.fits'%obsid,['image_%s_low_%i_SSDm.int.restored.fits' %(obsid,i) for i in range(len(omslist))],freqs)
+        if os.path.isfile('cube_%s.pybdsm.srl'%obsid):
+            warn('Source list exists, skipping source extraction')
+        else:
+            warn('Running PyBDSM, please wait...')
+            img=bdsm.process_image('%scube.fits'%obsid,thresh_pix=5,rms_map=True,atrous_do=True,atrous_jmax=2,group_by_isl=True,rms_box=(80,20), adaptive_rms_box=True, adaptive_thresh=80, rms_box_bright=(35,7),mean_map='zero',spectralindex_do=True,specind_maxchan=1,debug=True,kappa_clip=3,flagchan_rms=False,flagchan_snr=False,incl_chan=True,spline_rank=1)
+            # Write out in ASCII to work round bug in pybdsm
+            img.write_catalog(catalog_type='srl',format='ascii',incl_chan='true')
+            img.export_image(img_type='rms',img_format='fits')
 
-    # Now apply corrections
+        from make_fitting_product import make_catalogue
+        import fitting_factors
+        import find_outliers
 
-    if o['full_mslist'] is None:
-        die('Need big mslist to apply corrections')
-    if not(o['dryrun']):
-        warn('Applying corrections to MS list')
-        scale=np.load('crossmatch-results-2.npy')[:,0]
-        # InterpolatedUS gives us linear interpolation between points
-        # and extrapolation outside it
-        spl = InterpolatedUnivariateSpline(freqs, scale, k=1)
-        bigmslist=[s.strip() for s in open(o['full_mslist']).readlines()]
-        for ms in bigmslist:
-            t = pt.table(ms)
-            try:
-                dummy=t.getcoldesc('SCALED_DATA')
-            except RuntimeError:
-                dummy=None
-            t.close()
-            if dummy is not None:
-                warn('Table '+ms+' has already been corrected, skipping')
+        # generate the fitting product
+        if os.path.isfile(obsid+'crossmatch-1.fits'):
+            warn('Crossmatch table exists, skipping crossmatch')
+        else:
+    
+            t = pt.table(omslist[0]+ '/FIELD', readonly=True, ack=False)
+            direction = t[0]['PHASE_DIR']
+            ra, dec = direction[0]
+
+            if (ra<0):
+                ra+=2*np.pi
+            ra*=180.0/np.pi
+            dec*=180.0/np.pi
+
+            cats=zip(o['catalogues'],o['names'],o['groups'],o['radii'])
+            make_catalogue('%scube.pybdsm.srl'%obsid,ra,dec,2.5,cats)
+        
+        freqlist=open(obsid+'frequencies.txt','w')
+        for n,f in zip(o['names'],o['frequencies']):
+            freqlist.write('%f %s_Total_flux %s_E_Total_flux False\n' % (f,n,n))
+        for i,f in enumerate(freqs):
+            freqlist.write('%f Total_flux_ch%i E_Total_flux_ch%i True\n' % (f,i+1,i+1))
+        freqlist.close()
+
+        # Now call the fitting code
+
+        if os.path.isfile(obsid+'crossmatch-results-1.npy'):
+            warn('Results 1 exists, skipping first fit')
+        else:
+            if o['use_mpi']:
+                run('mpiexec -np 24 fitting_factors.py 1 %s'%obsid,dryrun=o['dryrun'],log=None,quiet=o['quiet'])
             else:
-                t = pt.table(ms+'/SPECTRAL_WINDOW', readonly=True, ack=False)
-                frq=t[0]['REF_FREQUENCY']
-                factor=spl(frq)
-                print frq,factor
-                t=pt.table(ms,readonly=False)
-                desc=t.getcoldesc(o['colname'])
-                desc['name']='SCALED_DATA'
-                t.addcols(desc)
-                d=t.getcol(o['colname'])
-                d*=factor
-                t.putcol('SCALED_DATA',d)
+                fitting_factors.run_all(1, name=obsid)
+
+        if os.path.isfile(obsid+'crossmatch-2.fits'):
+            warn('Second crossmatch exists, skipping outlier rejection')
+        else:
+            find_outliers.run_all(1, name=obsid)
+        
+        if os.path.isfile(obsid+'crossmatch-results-2.npy'):
+            warn('Results 1 exists, skipping second fit')
+        else:
+            if o['use_mpi']:
+                run('mpiexec -np 24 fitting_factors.py 2 %s'%obsid,dryrun=o['dryrun'],log=None,quiet=o['quiet'])
+            else:
+                fitting_factors.run_all(2, name=obsid)
+
+        # Now apply corrections
+
+        if o['full_mslist'] is None:
+            die('Need big mslist to apply corrections')
+        if not(o['dryrun']):
+            warn('Applying corrections to MS list')
+            scale=np.load(obsid+'crossmatch-results-2.npy')[:,0]
+            # InterpolatedUS gives us linear interpolation between points
+            # and extrapolation outside it
+            spl = InterpolatedUnivariateSpline(freqs, scale, k=1)
+            
+            bigmslist=[s.strip() for s in open(o['full_mslist']).readlines()]
+            obigmslist = [ms for ms in bigmslist if obsid in ms]
+            
+            for ms in obigmslist:
+                t = pt.table(ms)
+                try:
+                    dummy=t.getcoldesc('SCALED_DATA')
+                except RuntimeError:
+                    dummy=None
                 t.close()
+                if dummy is not None:
+                    warn('Table '+ms+' has already been corrected, skipping')
+                else:
+                    t = pt.table(ms+'/SPECTRAL_WINDOW', readonly=True, ack=False)
+                    frq=t[0]['REF_FREQUENCY']
+                    factor=spl(frq)
+                    print frq,factor
+                    t=pt.table(ms,readonly=False)
+                    desc=t.getcoldesc(o['colname'])
+                    desc['name']='SCALED_DATA'
+                    t.addcols(desc)
+                    d=t.getcol(o['colname'])
+                    d*=factor
+                    t.putcol('SCALED_DATA',d)
+                    t.close()
 
 if __name__=='__main__':
     from options import options
