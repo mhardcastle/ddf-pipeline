a66fed070f05be15f3d4bcabd3c5aca5da43e48c
Wendy L. Williams <wndywllms@gmail.com>
2017-02-03 17:20:07 +0000

    make_mslists for multiple observations

--- make_mslists.py
+++ make_mslists.py
@@ -19,12 +19,24 @@ def make_list():
         print ms,ff
         if ff<0.8:
             full_mslist.append(ms)
+    full_mslist = np.array(full_mslist)
+            
+    # check for multiple observations
+    obsids = np.array([ms.split('_')[0] for ms in full_mslist])
+    Uobsids = np.unique(obsids)
 
     if len(full_mslist)<24:
         warn('Warning -- only %i ms found' % len(full_mslist))
+        
+    # ensure lists contain all ms from all observations and same subset for each observation
+    write_full_mslist = np.array(())
+    write_mslist = np.array(())
+    for obsid in Uobsids:        
+        write_full_mslist = np.hstack((write_full_mslist,full_mslist[obsids==obsid]))
+        write_mslist = np.hstack((write_mslist,full_mslist[obsids==obsid][2::4]))
 
-    open('big-mslist.txt','w').writelines(ms+'\n' for ms in full_mslist)
-    open('mslist.txt','w').writelines(ms+'\n' for ms in full_mslist[2::4])
+    open('big-mslist.txt','w').writelines(ms+'\n' for ms in write_full_mslist)
+    open('mslist.txt','w').writelines(ms+'\n' for ms in write_mslist)
 
 if __name__=='__main__':
     make_list()
