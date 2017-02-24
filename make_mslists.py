2331906cf0b3edcca89c3bce79b2fe71fd5ebe69
Wendy L. Williams <wndywllms@gmail.com>
2017-02-24 15:27:09 +0000

    use start times instead of ms name to identify multiple observations

--- make_mslists.py
+++ make_mslists.py
@@ -7,23 +7,29 @@ import numpy as np
 from auxcodes import warn
 
 def check_flagged(ms):
-    t = pt.table(ms)
+    t = pt.table(ms, readonly=True)
     tc = t.getcol('FLAG').flatten()
     return float(np.sum(tc))/len(tc)
+
+def get_timerange(ms):
+    t = pt.table(ms +'/OBSERVATION', readonly=True, ack=False)
+    return t.getcell('TIME_RANGE',0)
 
 def make_list():
     g=sorted(glob.glob('*.ms'))
     full_mslist=[]
+    start_times=[]
     for ms in g:
         ff=check_flagged(ms)
+        t0,t1=get_timerange(ms)
         print ms,ff
         if ff<0.8:
             full_mslist.append(ms)
+            start_times.append(t0)
     full_mslist = np.array(full_mslist)
             
     # check for multiple observations
-    obsids = np.array([ms.split('_')[0] for ms in full_mslist])
-    Uobsids = np.unique(obsids)
+    Ustart_times = np.unique(start_times)
 
     if len(full_mslist)<24:
         warn('Warning -- only %i ms found' % len(full_mslist))
@@ -31,9 +37,9 @@ def make_list():
     # ensure lists contain all ms from all observations and same subset for each observation
     write_full_mslist = np.array(())
     write_mslist = np.array(())
-    for obsid in Uobsids:        
-        write_full_mslist = np.hstack((write_full_mslist,full_mslist[obsids==obsid]))
-        write_mslist = np.hstack((write_mslist,full_mslist[obsids==obsid][2::4]))
+    for start_time in Ustart_times:        
+        write_full_mslist = np.hstack((write_full_mslist,full_mslist[start_times==start_time]))
+        write_mslist = np.hstack((write_mslist,full_mslist[start_times==start_time][2::4]))
 
     open('big-mslist.txt','w').writelines(ms+'\n' for ms in write_full_mslist)
     open('mslist.txt','w').writelines(ms+'\n' for ms in write_mslist)
