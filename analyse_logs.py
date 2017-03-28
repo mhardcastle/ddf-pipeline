0aa7f73dabdddeb7ef2174710d316a972d3db411
Martin Hardcastle <mjh@extragalactic.info>
2017-03-28 22:54:55 +0100

    Analyse logs script

---
+++ analyse_logs.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+from datetime import datetime
+import glob
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_time_range(file):
+    lines=open(file).readlines()
+    start=datetime.strptime(lines[1][:19], '%Y-%m-%d %H:%M:%S')
+    end=datetime.strptime(lines[-2][:19], '%Y-%m-%d %H:%M:%S')
+    return (end-start).total_seconds()
+
+os.chdir('logs')
+g=glob.glob('*')
+times=[]
+for f in g:
+    dt=get_time_range(f)
+    times.append(dt)
+
+labels=['Miscellaneous','Wide-field KillMS','KillMS phase 60sb','KillMS amp/phase 60sb', 'KillMS amp/phase full', 'KillMS amp/phase full 2', 'Wide-field DDF dirin', 'DDF dirin', 'Predict wide-field', 'DDF phase 60sb', 'DDF amp-phase 60sb', 'DDF full', 'DDF full 2', 'DDF full low', 'DDF bootstrap', 'DDF bootstrap single-band']
+fragments=['***','wide_killms_p1','killms_p1','killms_ap1','killms_f_ap1','killms_f_ap2','wide_image_dirin', 'wide_image_phase1_predict', 'image_dirin','image_phase1','image_ampphase1','image_full_ampphase1','image_full_ampphase2','image_full_low','image_bootstrap','image_low']
+sums=np.zeros(len(labels))
+print len(labels),len(fragments)
+
+# classify each file
+for j,f in enumerate(g):
+    for i in range(len(labels)):
+        if fragments[i] in f:
+            label=i
+            break
+    else:
+        label=0
+    
+    print f,times[j],labels[label]
+    sums[label]+=times[j]
+
+print '------\n'
+
+pl=[]
+ps=[]
+print '---------------------------------\nOperation                 Time(s)\n---------------------------------'
+for i in range(len(labels)):
+    if sums[i]:
+        print "%-25s %7.0f" % (labels[i],sums[i])
+        pl.append(labels[i])
+        ps.append(sums[i])
+
+tt=np.sum(sums)
+print '---------------------------------\n\nTotal time %.0f seconds -- %.2f days' % (tt,tt/86400.0)
+
+cmap = plt.cm.spectral
+colors = cmap(np.linspace(0.1, 1., len(pl)))
+
+fig1, ax1 = plt.subplots()
+ax1.pie(ps,labels=pl,autopct='%1.1f%%',colors=colors)
+ax1.axis('equal')
+plt.show()
