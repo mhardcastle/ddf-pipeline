7c3f6c2bcf09f1590ffa6e4d9ccd4d18ee7a6868
Martin Hardcastle <mjh@extragalactic.info>
2017-05-08 13:45:24 +0100

    Add total number check to download

--- download.py
+++ download.py
@@ -14,25 +14,33 @@ def download_dataset(server,root):
     print page.headers['content-type']
     tree=html.fromstring(page.text)
     row = tree.xpath('//a')
+    files=[]
+    urls=[]
     for r in row:
         if r.text is not None and 'CAL' in r.text:
-            if os.path.isfile(r.text):
-                print 'File',r.text,'already exists, skipping'
-            else:
-                print 'Downloading',r.text
-                url=server+r.attrib['href']
-                while True:
-                    try:
-                        response = requests.get(url, stream=True,verify=False,timeout=300)
-                    except requests.exceptions.ConnectionError,requests.exceptions.Timeout:
-                        print 'Connection error! sleeping 60 seconds before retry...'
-                        sleep(60)
-                    else:
-                        break
-                with open(r.text, 'wb') as out_file:
-                    shutil.copyfileobj(response.raw, out_file)
-                del response
-            
+            files.append(r.text)
+            urls.append(r.attrib['href'])
+    if len(files)!=25:
+        print 'There should be 25 files! Check SARA manually.'
+        return False
+    for f,u in zip(files,urls):
+        if os.path.isfile(f):
+            print 'File',f,'already exists, skipping'
+        else:
+            print 'Downloading',f
+            url=server+u
+            while True:
+                try:
+                    response = requests.get(url, stream=True,verify=False,timeout=300)
+                except requests.exceptions.ConnectionError,requests.exceptions.Timeout:
+                    print 'Connection error! sleeping 60 seconds before retry...'
+                    sleep(60)
+                else:
+                    break
+            with open(r.text, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
+    return True
 
 if __name__=='__main__':
 
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Run pipeline download/unpack steps followed by the main job
 
-from auxcodes import report,warn
+from auxcodes import report,warn,die
 from download import download_dataset
 from unpack import unpack
 from make_mslists import make_list
@@ -24,7 +24,8 @@ except OSError:
     pass
 os.chdir(name)
 report('Downloading data')
-download_dataset('https://lofar-webdav.grid.sara.nl','/SKSP/'+name+'/')
+if not download_dataset('https://lofar-webdav.grid.sara.nl','/SKSP/'+name+'/'):
+    die('Download failed to get the right number of files')
 
 report('Unpacking data')
 unpack()
@@ -36,3 +37,5 @@ report('Making ms lists')
 if make_list():
     report('Submit job')
     os.system('qsub -N ddfp-'+name+' -v WD='+rootdir+'/'+name+' '+qsubfile)
+else:
+    die('make_list could not construct the MS list')
