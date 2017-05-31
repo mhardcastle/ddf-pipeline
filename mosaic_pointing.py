d5e875685bb7d5e1b8085a480e26def5a5a67683
Tim Shimwell <shimwell@lofar2.strw.leidenuniv.nl>
2017-05-31 22:10:27 +0200

    Typo fix in mosaic_pointing

--- mosaic_pointing.py
+++ mosaic_pointing.py
@@ -21,7 +21,7 @@ if __name__=='__main__':
     parser.add_argument('--directories', metavar='D', nargs='+',
                         help='directories to search for pipeline output')
     parser.add_argument('--beamcut', dest='beamcut', default=0.3, help='Beam level to cut at')
-    parset.add_argument('--no-check',dest='no_check', action='store_true', help='Do not check for missing images')
+    parser.add_argument('--no-check',dest='no_check', action='store_true', help='Do not check for missing images')
     parser.add_argument('pointingfile', type=str, help='LoTSS pointing progress file')
     parser.add_argument('mospointingname', type=str, help='Mosaic central pointing name')
     
