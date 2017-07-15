e9963087d78447a14da0672f31c0bfdbf00ee711
Martin Hardcastle <mjh@extragalactic.info>
2017-07-15 08:50:25 +0100

    killMS install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,6 @@ git clone https://github.com/cyriltasse/SkyModel.git
 git clone https://github.com/saopicc/killMS.git
 cd killMS
 git checkout lofar-stable
-git tag -a 'v1.0' -m 'Dummy tag'
 cd Predict
 make
 cd ../Array/Dot
@@ -19,4 +18,3 @@ git checkout lofar-stable
 python setup.py build
 cd $WD
 sed -e "s|INSTALLDIR|$WD|" ddf-pipeline/misc/DDF.sh > init.sh
-ln -s killMS killMS2
