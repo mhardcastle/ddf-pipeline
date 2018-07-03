b3fdcc6f3a16f2108ad09a50953f73581cd3d81a
Martin Hardcastle <mjh@extragalactic.info>
2018-07-03 22:18:27 +0100

    Polarization cube assembly

--- scripts/do_polcubes.py
+++ scripts/do_polcubes.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from pipeline import *
 from pyrap.tables import table
@@ -6,11 +5,47 @@ from auxcodes import getpos
 from astropy import units as u
 from astropy.coordinates import Angle
 
+# Utility functions for making the cube
+
+def get_freqs_hdus(filenames):
+    hdus=[]
+    freqs=[]
+    g=glob.glob(filenames)
+    for f in g:
+        hdus.append(fits.open(f))
+        header=hdus[-1][0].header
+        freqs.append(header['CRVAL4'])
+
+    freqs,hdus = (list(x) for x in zip(*sorted(zip(freqs, hdus), key=lambda pair: pair[0])))
+    return freqs,hdus
+    
+def make_cube(freqs,hdus,outfile):
+
+    chans=[]
+    for h in hdus:
+        ch,stokes,y,x=h[0].data.shape
+        chans.append(ch)
+        
+    newdata=np.zeros((np.sum(chans),stokes,y,x),dtype=np.float32)
+    print 'Output file shape is', newdata.shape
+    for i,h in enumerate(hdus):
+        if i==0:
+            chb=0
+        else:
+            chb=sum(chans[:i])
+        newdata[chb:chb+chans[i],:,:,:]=h[0].data
+
+    ohdu=hdus[0]
+    ohdu[0].data=newdata
+    ohdu[0].header['NAXIS4']=np.sum(chans)
+    hdus[0].writeto(outfile,clobber=True)
+
 def do_polcubes(colname,
-                         CurrentDDkMSSolName,
-                         uvrange,
-                         ddf_kw,
-                         options=None,catcher=None):
+                CurrentDDkMSSolName,
+                uvrange,
+                ddf_kw,
+                options=None,catcher=None):
+
     o=options
 
     mslist=o['full_mslist']
@@ -40,5 +75,20 @@ def do_polcubes(colname,
                   startchan=0,endchan=channels,options=o,
                   catcher=catcher)
 
+    outfile='image_full_low_QU.cube.dirty.fits'
+    if os.path.isfile(outfile):
+        warn('Uncorrected cube file already exists, not making it')
+    else:
+        report('Making uncorrected cube')
+        freqs,hdus=get_freqs_hdus('image_full_low_QU_Cube*.cube.dirty.fits')
+        make_cube(freqs,hdus,outfile)
 
+    outfile='image_full_low_QU.cube.dirty.corr.fits'
+    if os.path.isfile(outfile):
+        warn('Corrected cube file already exists, not making it')
+    else:
+        freqs,hdus=get_freqs_hdus('image_full_low_QU_Cube*.cube.dirty.corr.fits')
+        report('Making corrected cube')
+        make_cube(freqs,hdus,outfile)
+            
 
