3e5383f875a167212eeb5118e3a9e08688af7282
Martin Hardcastle <mjh@extragalactic.info>
2018-07-12 14:50:04 +0100

    Rework database code (other routines to follow)

--- utils/surveys_db.py
+++ utils/surveys_db.py
@@ -5,9 +5,9 @@ import MySQLdb.cursors as mdbcursors
 import os
 
 def get_next():
-    # return the name of the top-priority field with status 'Preprocessed'
+    # return the name of the top-priority field with appropriate status
     sdb=SurveysDB()
-    sdb.cur.execute('select * from fields where status="Preprocessed" order by priority desc')
+    sdb.cur.execute('select fields.id as id,sum(integration) as s,count(observations.id) as c,fields.priority from fields left join observations on (observations.field=fields.id) where fields.status="Not started" and observations.status="Preprocessed" group by fields.id having s>7 order by fields.priority desc,ra')
     results=sdb.cur.fetchall()
     sdb.close()
     return results[0]['id']
@@ -20,19 +20,16 @@ def update_status(name,status):
         # work it out
         id=get_id()
     else:
-        if isinstance(name,str):
-            id=int(name[1:])
-        else:
-            id=int(name)
-
+        id=name
+        
     sdb=SurveysDB()
-    idd=sdb.get_id(id)
+    idd=sdb.get_field(id)
     idd['status']=status
-    tag_idd(sdb,idd)
+    tag_field(sdb,idd)
     sdb.set_id(idd)
     sdb.close()
 
-def tag_idd(sdb,idd):
+def tag_field(sdb,idd):
     # Add location and user tags
     idd['clustername']=get_cluster()
     idd['location']=os.getcwd()
@@ -42,7 +39,7 @@ def tag_idd(sdb,idd):
 def get_id():
     dir=os.getcwd()
     dname=dir.split('/')[-1]
-    return int(dname[1:])
+    return dname
 
 def get_user():
     return os.getenv('USER')
@@ -60,7 +57,13 @@ def use_database():
 class SurveysDB(object):
     ''' Provides low-level and high-level interfaces to the surveys database '''
 
-    def __init__(self,localport=33306):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.close()
+
+    def __init__(self,localport=33306,readonly=False):
 
         # get the config file -- this must exist
         home=os.getenv("HOME")
@@ -71,6 +74,9 @@ class SurveysDB(object):
         except:
             self.ssh_user=None
         
+        # read only use
+        self.readonly=readonly
+
         # set up an ssh tunnel if not running locally
         self.usetunnel=False
         self.hostname=socket.gethostname()
@@ -93,12 +99,14 @@ class SurveysDB(object):
             else:
                 self.con = mdb.connect('lofar-server.data', 'survey_user', self.password, 'surveys')
         self.cur = self.con.cursor(cursorclass=mdbcursors.DictCursor)
-        self.cur.execute('lock table fields write')
+        if not self.readonly:
+            self.cur.execute('lock table fields write, observations write')
         self.closed=False
 
     def close(self):
         if not self.closed:
-            self.cur.execute('unlock tables')
+            if not self.readonly:
+                self.cur.execute('unlock tables')
             self.con.close()
             if self.usetunnel:
                 self.tunnel.stop()
@@ -107,8 +115,7 @@ class SurveysDB(object):
     def __del__(self):
         self.close()
         
-
-    def get_id(self,id):
+    def get_field(self,id):
         self.cur.execute('select * from fields where id=%s',(id,))
         result=self.cur.fetchall()
         if len(result)==0:
@@ -116,7 +123,8 @@ class SurveysDB(object):
         else:
             return result[0]
 
-    def set_id(self,sd):
+    def set_field(self,sd):
+        assert not self.readonly
         id=sd['id'];
         for k in sd:
             if k=='id':
@@ -124,13 +132,34 @@ class SurveysDB(object):
             if sd[k] is not None:
                 self.cur.execute('update fields set '+k+'=%s where id=%s',(sd[k],id))
 
-    def create_id(self,id):
+    def create_field(self,id):
         self.cur.execute('insert into fields(id) values (%s)',(id,))
-        return self.get_id(id)
+        return self.get_field(id)
+
+    def get_observation(self,id):
+        self.cur.execute('select * from observations where id=%s',(id,))
+        result=self.cur.fetchall()
+        if len(result)==0:
+            return None
+        else:
+            return result[0]
+
+    def set_observation(self,sd):
+        assert not self.readonly
+        id=sd['id'];
+        for k in sd:
+            if k=='id':
+                continue
+            if sd[k] is not None:
+                self.cur.execute('update observations set '+k+'=%s where id=%s',(sd[k],id))
+
+    def create_observation(self,id):
+        self.cur.execute('insert into observations(id) values (%s)',(id,))
+        return self.get_field(id)
 
 if __name__=='__main__':
     sdb=SurveysDB()
-    result=sdb.get_id(647109)
+    result=sdb.get_field('P35Hetdex10')
     #result['location']='Never Never Land'
     #sdb.set_id(result)
     print result
