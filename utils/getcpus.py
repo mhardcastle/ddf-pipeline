8c0f153ae5384306cccf14396a3b76572b866303
Martin Hardcastle <mjh@extragalactic.info>
2018-07-17 19:57:43 +0100

    Fix Cyril's fix

--- utils/getcpus.py
+++ utils/getcpus.py
@@ -2,20 +2,7 @@ import os
 import psutil
 
 def get_physical_cpus():
-    return psutil.cpu_count()
-    # find total number of physical cores, ignoring hyperthreading
-    lines=open('/proc/cpuinfo').readlines()
-    cpus=[]
-    for l in lines:
-        bits=l.split(':')
-        if 'physical id' in l:
-            phys=int(bits[1])
-        if 'core id' in l:
-            core=int(bits[1])
-        if l=='\n':
-            cpus.append((phys,core))
-    cpus=set(cpus)
-    return len(cpus)
+    return psutil.cpu_count(logical=False)
 
 def getcpus():
     nodefile=os.getenv('PBS_NODEFILE')
