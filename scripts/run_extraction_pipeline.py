54dee12e55accf4aafa52932a2ee7bcbeadf2606
Tim Shimwell <shimwell@lofar2.strw.leidenuniv.nl>
2019-02-09 08:35:21 +0100

    Added in final copy to the run_extraction_pipeline

--- scripts/run_extraction_pipeline.py
+++ scripts/run_extraction_pipeline.py
@@ -31,7 +31,7 @@ def create_ds9_region(filename,ra,dec,size):
     return(filename)
 
 
-def do_run_subtract(name,basedir,archivedir):
+def do_run_subtract(name,basedir,inarchivedir,outarchivedir):
     startdir = os.getcwd()
     sdb=SurveysDB()
     extractdict = sdb.get_reprocessing(name)
@@ -44,7 +44,6 @@ def do_run_subtract(name,basedir,archivedir):
     for i in range(0,len(fields)):
         if extract_status[i] != 'EREADY':
             continue
-
         field = fields[i]
 
         workdir=basedir+'/'+name
@@ -63,10 +62,10 @@ def do_run_subtract(name,basedir,archivedir):
         sdb.close()
         print 'Updated status to STARTED for',field,name
         time.sleep(2.0)
-        report('Copying data from %s'%archivedir)
+        report('Copying data from %s'%inarchivedir)
         
         # WANT TO MAKE THIS INTO A RSYNC SO THAT IT CAN BE DONE OUTSIDE LEIDEN
-        os.system('cp -r %s/%s %s'%(archivedir,field,workdir))
+        os.system('cp -r %s/%s %s'%(inarchivedir,field,workdir))
         
         # Update status to copied here
         extract_status[i] = 'COPIED'
@@ -88,7 +87,10 @@ def do_run_subtract(name,basedir,archivedir):
         os.system('sub-sources-outside-region.py -b %s/%s.ds9.reg -p %s'%(workdir,name,name))
 
         # Archive the results need an rsync code this is just the *archive file that needs to be archived.
-        #os.sys(cp -r
+        os.system('mkdir %s/%s'%(outarchivedir,name))
+        os.system('mkdir %s/%s/%s'%(outarchivedir,name,field))
+        print  ('cp -r %s_%s.dysco.sub.shift.avg.weights.ms.archive %s/%s/%s'%(field,name,outarchivedir,name,field))
+        os.system('cp -r %s_%s.dysco.sub.shift.avg.weights.ms.archive %s/%s/%s'%(field,name,outarchivedir,name,field))
 
 
         # update the database to give success
@@ -103,4 +105,4 @@ def do_run_subtract(name,basedir,archivedir):
 if __name__=='__main__':
     target = get_next_extraction()['id']
     # Takes the targetname, the current directory (the working directory), and the directory that contains the LoTSS-DR2 archive
-    do_run_subtract(target,os.getcwd(),'/disks/paradata/shimwell/LoTSS-DR2/archive/')
+    do_run_subtract(target,os.getcwd(),'/disks/paradata/shimwell/LoTSS-DR2/archive/','/disks/paradata/shimwell/LoTSS-DR2/archive_extract_test/')
