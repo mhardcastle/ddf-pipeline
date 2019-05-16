74ef5de965a7873ecf21b68d657c12734e97ed8b
Martin Hardcastle <mjh@extragalactic.info>
2019-05-16 12:36:01 +0100

    Add auto-QP running to the Herts DR2 release scripts

--- scripts/make_dr2_release.py
+++ scripts/make_dr2_release.py
@@ -150,6 +150,8 @@ if __name__=='__main__':
                 outfile.write('<tr><td>%s</td><td>%.3f</td><td>%.3f</td><td>%s</td><td>%s, %s</td><td>%s, %s</td><td>%s, %s, %s</td><td>%s</td><td>%s, %s, %s, %s</td></tr>\n' % (id,r['ra'],r['decl'],r['end_date'],fint,fapp,lowint,lowapp,band[0],band[1],band[2],stokesv,stokesqu,stokesquvlow,stokesqu_app,stokesquvlow_app))
 
         outfile.close()
+        separator('Quality pipeline')
+        os.system('queue_quality.py')
         separator('Sleeping')
 
         sleep(7200)
