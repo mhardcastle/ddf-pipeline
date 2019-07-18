1b554b4a6e7cf85c1ab9aa89d5fa668053165b6a
Martin Hardcastle <mjh@extragalactic.info>
2019-07-18 17:40:56 +0100

    Add DR2 area script

---
+++ scripts/find_dr2_area.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+from astropy_healpix import HEALPix
+from astropy import units as u
+
+pos=np.loadtxt(os.environ['DDF_DIR']+'/ddf-pipeline/misc/DR2-pointings.txt',usecols=(1,2))
+
+pos=pos[(pos[:,0]>137) & (pos[:,0]<250)]
+
+print 'Number of pointings is',len(pos)
+ra=pos[:,0]
+dec=pos[:,1]
+
+print 'RA range is',np.min(ra),np.max(ra)
+print 'Dec range is',np.min(dec),np.max(dec)
+
+hp = HEALPix(nside=1024)
+print hp.npix,'total healpix pixels on sky'
+area=hp.pixel_area.value*3283
+
+plist=[]
+
+for i in range(len(ra)):
+    pixels=hp.cone_search_lonlat(ra[i]*u.deg,dec[i]*u.deg,1.85*u.deg)
+    plist=plist+list(pixels)
+
+print len(plist),'total pixels'
+print len(set(plist)),'total distinct pixels'
+print 'Area is',len(set(plist))*area,'sq. deg'
+
+
