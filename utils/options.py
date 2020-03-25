7ffaa15ced7a577c12a756c4d64538354c54fc82
Martin Hardcastle <mjh@extragalactic.info>
2020-03-25 16:39:33 +0000

    fix configparser

--- utils/options.py
+++ utils/options.py
@@ -5,7 +5,10 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 #from builtins import str
-import configparser
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
 import os
 import re
 import sys
