8d88e7b62c93f8be5696ff9bcaa9f2628e2a65e7
Tim Shimwell <lotss-tshimwell@ui-01.spider.surfsara.nl>
2021-09-24 16:28:58 +0200

    Updated run_extraction_pipeline.py to run with singularity and rclone

--- scripts/run_extraction_pipeline.py
+++ scripts/run_extraction_pipeline.py
@@ -4,12 +4,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from builtins import range
-from auxcodes import report,warn,die
 from surveys_db import *
-from download import download_dataset
-from download_field import download_field
-
-from auxcodes import MSList
 import sys
 import os
 import glob
@@ -19,42 +14,27 @@ import time
 from subprocess import call
 from rclone import RClone
 
-def do_rclone_upload(cname,basedir,f):
+macaroonpath = '/project/lotss/Software/ddf-operations/testing/'
+
+def do_rclone_upload(cname,basedir,f,directory):
     '''
     Untested upload code
     '''
-    rc=Rclone('add-your-macaroon-here.conf',debug=True)
-    rc.multicopy(basedir,f,rc.remote+'destination-directory/'+cname)
-
-def do_rsync_upload(cname,basedir,f):
-    workdir=basedir+'/'+cname
-
-    #if os.environ['DDF_PIPELINE_CLUSTER']!='paracluster':
-    target='lofararchive@ssh.strw.leidenuniv.nl:'
-    #else:
-    #    target=''
-
-    while True:
-        s= 'rsync -avz --relative --progress --perms --chmod=ugo+rX --safe-links --partial --timeout=20 '+' '.join(f)+' '+target+'/disks/paradata/shimwell/LoTSS-DR2/archive_extract/'+cname 
-        print('Running command:',s)
-        retval=call(s,shell=True)
-        if retval==0:
-            break
-        print('Non-zero return value',retval)
-        if retval!=30:
-            raise RuntimeError('rsync failed unexpectedly')
-        time.sleep(10)
+    rc=RClone('%s/maca_sksp_disk_extract.conf'%macaroonpath,debug=True)
+    rc.get_remote()
+    print(rc.remote,'%s/maca_sksp_disk_extract.conf'%macaroonpath)
+    rc.multicopy(basedir,f,rc.remote+directory+'/'+cname)
 
 def do_rclone_download(cname,f):
     '''
     Download required data from field cname into location f
     '''
     tarfiles=['images.tar','uv.tar']
-    for macaroon, directory in [('maca_sksp_tape_DR2_readonly.conf',''),('maca_sksp_tape_DDF.conf','archive/')]:
+    for macaroon, directory in [('%s/maca_sksp_tape_DR2_readonly.conf'%macaroonpath,''),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'archive/'),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'other/')]:
         try:
             rc=RClone(macaroon,debug=True)
         except RuntimeError:
-            print('Macaroon',m,'does not exist!')
+            print('Macaroon',macaroon,'does not exist!')
             continue
         rc.get_remote()
         d=rc.multicopy(rc.remote+directory+cname,tarfiles,f)
@@ -79,27 +59,6 @@ def do_rclone_download(cname,f):
         if d!=0:
             raise RuntimeError('untar %s failed!' % t)
 
-def do_rsync_download(cname,basedir,f):
-    workdir=basedir+'/'+cname
-
-    #if os.environ['DDF_PIPELINE_CLUSTER']!='paracluster':
-    target='lofararchive@ssh.strw.leidenuniv.nl:'
-    #else:
-    #    target=''
-
-    while True:
-	excludeinclude = ' --include="image_full_ampphase_di_m.NS.mask01.fits" --include="image_full_ampphase_di_m.NS.app.restored.fits" --exclude="*QU_*" --exclude="*fits*" --exclude="*.tgz*" --exclude="*QU_*" --exclude="*DDS0*" --exclude="*DDS1*" --exclude="*DDS2*" --exclude="*.corrupted" '
-        s= 'rsync -azvh --timeout=20 --progress --perms --chmod=a+rwx'+ excludeinclude + target+workdir + ' ' + f
-        #'cd '+workdir+'; rsync -avz --progress --safe-links --inplace --append --partial --timeout=20 '+' '.join(f)+' '+target+'/disks/paradata/shimwell/LoTSS-DR2/archive/'+name
-        print('Running command:',s)
-        retval=call(s,shell=True)
-        if retval==0:
-            break
-        print('Non-zero return value',retval)
-        if retval!=30:
-            raise RuntimeError('rsync failed unexpectedly')
-        time.sleep(10)
-
 def create_ds9_region(filename,ra,dec,size):
 
     sc=SkyCoord('%sdeg'%ra,'%sdeg'%dec,frame='icrs')
@@ -114,115 +73,87 @@ def create_ds9_region(filename,ra,dec,size):
     return(filename)
 
 
-def do_run_subtract(name,basedir,inarchivedir,outarchivedir,force=False):
-    startdir = os.getcwd()
-    sdb=SurveysDB()
-    extractdict = sdb.get_reprocessing(name)
-    sdb.close()
-    fields = extractdict['fields'].split(',')
-    extract_status = extractdict['extract_status'].split(',')
-    try:
-        bad_pointings = extractdict['bad_pointings'].split(',')
-    except AttributeError:
-        bad_pointings = ['']
-    print('Working on ',name, 'in fields', fields,'which have status',extract_status)
-    
-    for i in range(0,len(fields)):
-        os.chdir(startdir)
-        if not(extract_status[i] == 'EREADY' or (force and extract_status[i] == 'STARTED')):
-            continue
-        field = fields[i]
-        if field in bad_pointings:
-            print('Field',field,'in bad pointings -- skipping and setting to BADP')
-            sdb=SurveysDB()
-            extractdict = sdb.get_reprocessing(name)
-            extract_status[i] = 'BADP'
-            extractdict['extract_status'] = ','.join(extract_status)
-            sdb.db_set('reprocessing',extractdict)
-            sdb.close()
-            continue
-        workdir=basedir+'/'+name
-        try:
-            os.mkdir(workdir)
-        except OSError:
-            warn('Working directory already exists')
-        print('In directory', os.getcwd())
-        os.chdir(workdir)
-        # Update status to running here
-        extract_status[i] = 'STARTED'
-        sdb=SurveysDB()
-        extractdict = sdb.get_reprocessing(name)
-        extractdict['extract_status'] = ','.join(extract_status)
-        sdb.db_set('reprocessing',extractdict)
-        sdb.close()
-        print('Updated status to STARTED for',field,name)
-        time.sleep(2.0)
-        report('Copying data from %s'%inarchivedir)
-        
-        # WANT TO MAKE THIS INTO A RSYNC SO THAT IT CAN BE DONE OUTSIDE LEIDEN
-        #os.system('cp -r %s/%s %s'%(inarchivedir,field,workdir))
-        do_rsync_download(field,inarchivedir,workdir)
+def stage_field_data(field):
 
-        # Update status to copied here
-        extract_status[i] = 'COPIED'
-        sdb=SurveysDB()
-        extractdict = sdb.get_reprocessing(name)
-        extractdict['extract_status'] = ','.join(extract_status)
-        sdb.db_set('reprocessing',extractdict)
-        sdb.close()
-        print('Updated status to COPIED for',field,name)
+    for macaroon, directory in [('%s/maca_sksp_tape_DR2_readonly.conf'%macaroonpath,''),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'archive/'),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'other/')]:
+         print('ada --tokenfile %s --stage %s/%s'%(macaroon,directory,field))
+         os.system('ada --tokenfile %s --stage %s/%s'%(macaroon,directory,field))
+    return
+
+def unstage_field_data(field):
+
+    for macaroon, directory in [('%s/maca_sksp_tape_DR2_readonly.conf'%macaroonpath,''),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'archive/'),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'other/')]:
+         print('ada --tokenfile %s --unstage %s/%s'%(macaroon,directory,field))
+         os.system('ada --tokenfile %s --unstage %s/%s'%(macaroon,directory,field))
+    return
+
+def check_staged(field):
+
+   notstaged = True
+   while notstaged:
+       for macaroon, directory in [('%s/maca_sksp_tape_DR2_readonly.conf'%macaroonpath,''),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'archive/'),('%s/maca_sksp_tape_DDF.conf'%macaroonpath,'other/')]:
+           print('ada --tokenfile %s --longlist %s/%s'%(macaroon,directory,field))
+           stageinfo = os.popen('ada --tokenfile %s --longlist %s/%s'%(macaroon,directory,field)).read().split()
+           numofnotstaged = stageinfo.count('NEARLINE')
+           if numofnotstaged == 0:
+              notstage = False
+              return
+           else:
+              print(stageinfo)
+              print('%s not staged'%numofnotstaged)
+              time.sleep(600)
 
 
-        # Create boxfile
-        create_ds9_region('%s.ds9.reg'%name,extractdict['ra'],extractdict['decl'],extractdict['size'])
-
+def do_run_extract(field,name):
 
         # Run subtract code
-        print(os.getcwd(), 'working here')
+        os.chdir(startdir)
+        os.chdir(name)
         os.chdir(field)
-        print ('sub-sources-outside-region.py -b %s/%s.ds9.reg -p %s'%(workdir,name,name))
-        result=os.system('sub-sources-outside-region.py -b %s/%s.ds9.reg -p %s'%(workdir,name,name))
+        print ('sub-sources-outside-region.py -b %s/%s.ds9.reg -p %s'%(startdir,name,name,name))
+        result=os.system('sub-sources-outside-region.py -b %s.ds9.reg -p %s'%(workdir,name,name))
         if result!=0:
             raise RuntimeError('sub-sources-outside-region.py failed with error code %i' % result)
-        
-        # Archive the results need an rsync code this is just the *archive file that needs to be archived.
-        #os.system('mkdir %s/%s'%(outarchivedir,name))
-        #os.system('mkdir %s/%s/%s'%(outarchivedir,name,field))
-        os.chdir(workdir)
-        f = glob.glob('%s/*.archive*'%(field))
-        do_rsync_upload(name,field,f)
-
-        #print  ('cp -r %s_%s.dysco.sub.shift.avg.weights.ms.archive %s/%s/%s'%(field,name,outarchivedir,name,field))
-        #os.system('cp -r %s_%s.dysco.sub.shift.avg.weights.ms.archive %s/%s/%s'%(field,name,outarchivedir,name,field))
-
-
-        # update the database to give success
-        extract_status[i] = 'EDONE'
-        sdb=SurveysDB()
-        extractdict = sdb.get_reprocessing(name)
-        extractdict['extract_status'] = ','.join(extract_status)
-        sdb.db_set('reprocessing',extractdict)
-        sdb.close()
-        print('Updated status to EDONE for',field,name)
-
-    # update the database to give selfcal status as SREADY
-    selfcal_status = 'SREADY'
-    sdb=SurveysDB()
-    extractdict = sdb.get_reprocessing(name)
-    extractdict['selfcal_status'] = selfcal_status
-    sdb.db_set('reprocessing',extractdict)
-    sdb.close()
-    print('Updated status to SREADY for',name)
 
 if __name__=='__main__':
 
     if len(sys.argv)==1:
-        target = get_next_extraction()['id']
+        target,field,ra,dec,size = get_next_extraction()
         force = False
     else:
         force = True
         target = sys.argv[1]
 
-    # Takes the targetname, the current directory (the working directory), and the directory that contains the LoTSS-DR2 archive
-    do_run_subtract(target,os.getcwd(),'/disks/paradata/shimwell/LoTSS-DR2/archive/','/disks/paradata/shimwell/LoTSS-DR2/archive_extract/',force=force)
+    startdir = os.getcwd()
+    os.system('mkdir %s'%target)
+    os.chdir(target)  
+
+    # Stage and wait for coming online
+    stage_field_data(field)
+    check_staged(field)
+
+    update_reprocessing_extract(target,field,'STARTED')
+
+    do_rclone_download(field,startdir+'/'+target + '/'+field)
+    unstage_field_data(field)
+    os.chdir(field)
+    create_ds9_region('%s.ds9.reg'%(target),ra,dec,size)
+
+    singularityfile = '/project/lotss/Software/lofar_sksp_fedora31_ddf.sif'
+
+    executionstr = 'singularity exec -B  /scratch/,/home/lotss-tshimwell/,/project/lotss/ %s sub-sources-outside-region.py -b %s.ds9.reg -p %s'%(singularityfile,target,target)
+    print(executionstr)
+    os.system(executionstr)
+
+    resultfiles = glob.glob('*archive*')
+    resultfilestar = []
+    for resultfile in resultfiles:
+        d=os.system('tar -cvf %s.tar %s'%(resultfile,resultfile))
+        if d!=0:
+            raise RuntimeError('Tar of %s failed'%resultfile)	
+        resultfilestar.append('%s.tar'%resultfile)
+
+    do_rclone_upload(field,os.getcwd(),resultfilestar,target)
+
+    update_reprocessing_extract(target,field,'EDONE')
 
