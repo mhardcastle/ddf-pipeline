943beaf53055cc87e1f0075edda62df61721257a
Martin Hardcastle <mjh@extragalactic.info>
2021-12-12 11:24:34 +0000

    Fix SDR indexing issue

--- utils/sdr_wrapper.py
+++ utils/sdr_wrapper.py
@@ -11,7 +11,9 @@ from time import sleep
 
 class SDR(object):
 
-    def __init__(self,url='https://tdr-test.surfsara.nl/api/objects/lotss-dr2/',target='.'):
+    def __init__(self,url='https://repository.surfsara.nl/api/objects/lotss-dr2/',target='.'):
+        if 'SDR_URL' in os.environ:
+            url=os.environ['SDR_URL']
         self.url=url
         if 'SDR_TOKEN' in os.environ:
             self.token=os.environ['SDR_TOKEN']
@@ -27,29 +29,45 @@ class SDR(object):
     def fc(self,field):
         ''' Convert a field name from LoTSS format '''
         return field.replace('+','-')
-            
-    def get_status(self,field):
+
+    def get_files(self,field):
+        # return mapping of file name to number
         field=self.fc(field)
-        r=requests.get(self.url+field+'/status'+self.tokenstr)
+        r=requests.get(self.url+field+'/files'+self.tokenstr,timeout=60)
         if r.status_code!=200:
             raise RuntimeError('Failed to get status! (Probably non-existent field')
         else:
             j=json.loads(r.text)
             files={}
             for i,d in enumerate(j):
-                files[d['name']]=(d['status'],i)
+                files[d['name']]=i
+            return files
+        
+    def get_status(self,field):
+        # return staging status of all files
+        field=self.fc(field)
+        r=requests.get(self.url+field+'/status'+self.tokenstr,timeout=60)
+        if r.status_code!=200:
+            raise RuntimeError('Failed to get status! (Probably non-existent field')
+        else:
+            j=json.loads(r.text)
+            files={}
+            for d in j:
+                files[d['name']]=d['status']
             return files
 
     def stage(self,field,filename):
         field=self.fc(field)
-        files=self.get_status(field)
-        if filename not in files:
+        files=self.get_files(field)
+        status=self.get_status(field)
+        if filename not in files or filename not in status:
             raise RuntimeError('Staging nonexistent file')
-        status,number=files[filename]
+        status=status[filename]
+        number=files[filename]
         if status=='DUL':
             return 'Online'
         else:
-            r=requests.post(self.url+field+'/stage/'+str(number)+self.tokenstr)
+            r=requests.post(self.url+field+'/stage/'+str(number)+self.tokenstr,timeout=60)
             return 'Staged'
         
     def download(self,field,filename):
@@ -69,7 +87,7 @@ class SDR(object):
             if f not in files:
                 raise RuntimeError('File not found!')
             else:
-                if files[f][0]=='OFL':
+                if files[f]=='OFL':
                     self.stage(field,f)
 
         print('Waiting for files to be online:')
@@ -78,7 +96,7 @@ class SDR(object):
             files=self.get_status(field)
             count=0
             for f in filenames:
-                if files[f][0]=='DUL':
+                if files[f]=='DUL':
                     count+=1
             print('%i/%i... ' % (count,len(filenames)),end='')
             sys.stdout.flush()
