044bfaa2bcdcb0c0cbf9f86e2fc52f3078fabb4b
AlexKurek <aleksander.kurek@uj.edu.pl>
2022-11-25 16:05:19 +0100

    Update reprocessing_utils.py

--- utils/reprocessing_utils.py
+++ utils/reprocessing_utils.py
@@ -64,7 +64,7 @@ def do_sdr_and_rclone_download(cname,f,verbose=False):
         status=None
     if status:
         if verbose: print('Initiating SDR download for field',cname)
-        tarfiles=['images.tar','uv*.tar']
+        tarfiles=['images.tar','uv.tar']
         s.download_and_stage(cname,tarfiles)
         tarfiles = glob.glob('*tar')
         untar(f,tarfiles,verbose=verbose)
@@ -76,7 +76,7 @@ def do_rclone_download(cname,f,verbose=False):
     '''
     Download required data from field cname into location f
     '''
-    tarfiles=['images.tar','uv*.tar']
+    tarfiles=['images.tar','uv.tar']
     for macaroon, directory in [('maca_sksp_tape_DR2_readonly.conf',''),('maca_sksp_tape_DDF.conf','archive/'),('maca_sksp_tape_DDF_readonly.conf','other/')]:
         try:
             rc=RClone(macaroon,debug=True)
