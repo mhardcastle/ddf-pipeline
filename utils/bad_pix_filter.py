f05a894a562a8828b95d07d41354e9464d81c3d2
Martin Hardcastle <mjh@extragalactic.info>
2025-02-28 07:22:23 +0000

    Tweak bad pix code

--- utils/bad_pix_filter.py
+++ utils/bad_pix_filter.py
@@ -3,17 +3,17 @@ import scipy.ndimage as nd
 import numpy as np
 import os
 import sys
+from copy import copy
 
 def make_blank_mask(indata,sizecut=20):
     '''
     take as input indata, a 2D array which may have small non-NaN islands in a sea of NaN, and return a mask of the same shape as indata which selects all those islands for NaN-blanking
     '''
-    data = indata.byteswap().newbyteorder()
+    data=copy(indata)
     data[data==0]==-9999
     data[np.isnan(data)]=0
     l,n=nd.label(data)
     size = np.bincount(l.ravel())
-    #biggest_label = size[1:].argmax() + 1
     big=np.arange(len(size))[size>sizecut]
     big=big[1:]
     return ~np.isin(l,big) 
@@ -21,7 +21,8 @@ def make_blank_mask(indata,sizecut=20):
 def blanker(infile,verbose=False):
 
     hdu=fits.open(infile)
-    mask=blank_mask(hdu[0].data)
+    swapdata = hdu[0].data.byteswap().newbyteorder()
+    mask=make_blank_mask(swapdata)
     hdu[0].data[mask]=np.nan
 
     os.rename(infile,infile.replace('.fits','-old.fits'))
