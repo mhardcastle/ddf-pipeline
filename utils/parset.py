51cbcb083e5ae6c3adb718693c6762639ae3537d
mathiscertenais <mathis.certenais@irisa.fr>
2025-07-02 16:58:58 +0200

    Create kMS MPI option 'mpi', 'kms_nproc' in utils/parset.py

--- utils/parset.py
+++ utils/parset.py
@@ -119,4 +119,5 @@ option_list = ( ( 'machine', 'NCPU_DDF', int, getcpus(),
                 ( 'inputmodel',  'basedicomodel',str,None,'Input dicomodel for calibration'),
                 ( 'inputmodel',  'basemaskname',str,None,'Input mask for calibration'),
                 ( 'inputmodel',  'baseimagename',str,None,'Input image for calibration'),
-                ( 'mpi', 'ddf_nproc', int, 1,'Number of processes for DDFacet MPI execution'))
+                ( 'mpi', 'ddf_nproc', int, 1,'Number of processes for DDFacet MPI execution'),
+                ( 'mpi', 'kms_nproc', int, 1,'Number of processes for killMS MPI execution'))
