3f7238ac63a325c4ec235cca3dfec8b1c9011d19
cyriltasse <cyril_tasse@yahoo.fr>
2025-10-23 14:07:20 +0000

    ok

---
+++ scripts/SplitInFreq.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+from pyrap.tables import table
+import sys
+import numpy as np
+
+
+def split(MSName):
+    tf=table("%s/SPECTRAL_WINDOW/"%MSName)
+    _,nch=tf.getcol("CHAN_FREQ").shape
+
+    nMS=24
+
+    chStep=nch//24
+    chEdges=np.linspace(0,nch,nMS+1)
+    TemplateName="DP3_split.parset"
+    f=open(TemplateName,"w")
+    f.write("msin.datacolumn = DATA\n")
+    f.write("msin.autoweight = false\n")
+    f.write("steps = []\n")
+    f.close()
+    
+
+    for iMS in range(nMS):
+        ch0,ch1=chEdges[iMS],chEdges[iMS+1]
+        nchThis=ch1-ch0
+        ss="DP3 %s msin=%s msout=%s.ch%i_%i.ms msin.startchan=%i msin.nchan=%i"%(TemplateName,MSName,MSName,ch0,ch1,ch0,nchThis)
+        print("===============================================")
+        print(ss)
+        os.system(ss)
+
+if __name__=="__main__":
+    MSName=sys.argv[1]
+    split(MSName)
