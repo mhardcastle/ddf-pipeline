a39f31af3a01ea0142602758b44d131bc9dec915
David Guibert <david.guibert@gmail.com>
2026-06-19 15:57:43 +0200

    install_uv: update for python 3.12

--- install_uv.sh
+++ install_uv.sh
@@ -8,23 +8,24 @@ fi
 
 
 # Use on HPC centers (Jean Zay, Adastra, ...)
-#export UV_CACHE_DIR=$PWD/uv_cache
+export UV_CACHE_DIR=$PWD/uv_cache
 # Use on Adastra HPC center
 #export CC=gcc
 
 # download DDFacet/killMS and ddf-pipeline
 # Use https method on some HPC centers
 export METHOD_CLONE="https://github.com/"
-export METHOD_CLONE="git@github.com:"
+#export METHOD_CLONE="git@github.com:"
 
 
-git clone ${METHOD_CLONE}cyriltasse/DDFacet -b HackathonRennes  ../DDFacet || true
-git clone ${METHOD_CLONE}cyriltasse/killMS -b HackathonRennes ../killMS || true
+git clone ${METHOD_CLONE}cyriltasse/DDFacet -b HackathonRennes_June26  ../DDFacet || true
+git clone ${METHOD_CLONE}cyriltasse/killMS -b HackathonRennes_June26 ../killMS || true
 # git clone ${METHOD_CLONE}dguibert/ddf-pipeline -b Hackaton_mpipool_test_NancepMPI_Herts ../ddf-pipeline || true
 
-uv venv -p 3.10
+uv venv -p 3.12
 source .venv/bin/activate
 uv sync --extra mpi-support --refresh-package ddfacet --refresh-package killms --active --verbose
-uv pip install https://github.com/dguibert/LOFARBeam/releases/download/v0.1-10-gc49afaf/lofarbeam-0.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+# Go to https://github.com/dguibert/LOFARBeam/releases/tag/v0.1-10-gc49afaf to find lofarbeam wheels
+uv pip install https://github.com/dguibert/LOFARBeam/releases/download/v0.1-10-gc49afaf/lofarbeam-0.0.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 
 
