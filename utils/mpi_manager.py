da7f8caafa7024e7ecc0123b2df3e02eab78bf9e
David Guibert <david.guibert@gmail.com>
2026-06-25 09:31:59 +0200

    mslist: append @0 when only the hostname is defined

--- utils/mpi_manager.py
+++ utils/mpi_manager.py
@@ -1,3 +1,10 @@
+MPIsize = 0
+RANK=0
+localrank = 0 # local process ID on the node
+localsize = 1 # number of mpi process on the node
+hostname = "localhost"
+USE_MPI=False
+
 try:
     from mpi4py import MPI
     MPIsize = MPI.COMM_WORLD.size
@@ -6,14 +13,19 @@ try:
     localrank = shared_comm.rank
     localsize = shared_comm.size
     hostname = MPI.Get_processor_name()
-except:
-    MPIsize = 0
-    RANK=0
-    localrank = 0 # local process ID on the node
-    localsize = 1 # number of mpi process on the node
-    hostname = "localhost"
 
-USE_MPI=(MPIsize>1)
+    if size>1:
+        USE_MPI=True
+    else:
+        print(ModColor.Str(" mpi4py properly initialised, but size=1, not using MPI mode.",col="blue"))
+
+except ModuleNotFoundError:
+    print(ModColor.Str(" Could not initialise mpi4py ",col="blue"))
+    pass
+except Error as e:
+    raise RuntimeError(e)
+
+
 LIST_SITES_BEING_USED=None
 
 if USE_MPI:
@@ -26,7 +38,7 @@ if USE_MPI:
     LIST_SITES_BEING_USED=hosts_rank
     print(f"LIST_SITES_BEING_USED={LIST_SITES_BEING_USED}")
 
-    
+
 import itertools
 import os
 from DDFacet.Other import ModColor
@@ -40,7 +52,7 @@ def CheckDistributedFS():
     cwd = os.getcwd()
     cwd = os.path.realpath(cwd)
     print(cwd)
-    
+
     best_match = ""
     fs_type = None
 
@@ -68,12 +80,15 @@ class MSSet():
         self.file_nodes_mslist=mslist
         # for MPI use
         mslist=[s.strip() for s in open(mslist).readlines()]
-        
+
         self.DicoMSName2Node = {}
         nodes2ms = {}
         for iMS,sMS in enumerate(mslist):
             if ":" in sMS:
                 node,msname=sMS.split(":")
+                # if no core specified, assume @0
+                if len(node.split("@")) == 1:
+                    node = f"{node}@0"
             else:
                 node,msname=None,sMS
             l=nodes2ms.get(node,[])
@@ -83,12 +98,12 @@ class MSSet():
 
         if (None in nodes2ms.keys()) and len(nodes2ms) == 1 and MPIsize>1:
             # get all node names because None have been specified
-            
+
             self.ListNodesBeingUsed = LIST_SITES_BEING_USED
-                
+
             mslist=list(zip(itertools.cycle(LIST_SITES_BEING_USED), nodes2ms[None]))
             del nodes2ms[None]
-            
+
             for node,ms in mslist:
                 l=nodes2ms.get(node,[])
                 l.append(ms)
@@ -97,27 +112,27 @@ class MSSet():
 
         self.DicoNodes2ListMS=nodes2ms
         self.ListNodesBeingUsed = list(nodes2ms.keys())
- 
+
         print(ModColor.Str(" Data set distribution ".center(WIDTH_PROMPT,"="),col="blue"))
         print(ModColor.Str((" %s "%(str(self.ListNodesBeingUsed))).center(WIDTH_PROMPT,"="),col="blue"))
         pprint.pp(self.DicoNodes2ListMS)
 
-        
+
 def testFunc(*args,**kwargs):
     host = hostname
     print(host,args,kwargs)
-    
+
 import os
 def testParallel():
     ListJobs=[["nancep10.obs-nancay.fr@0",testFunc,(5,),{"e":6}],
               ["nancep10.obs-nancay.fr@0",testFunc,(9,),{"f":6}],
               ["nancep11.obs-nancay.fr@0",testFunc,(77,),{"g":88}],
               ]
-    
+
     ListJobs=[["cw10055@0",os.system,("CleanSHM.py",), {}],
               ["cw10057@0",os.system,("CleanSHM.py",), {}],
               ]
-    
+
     ListJobs=[["nancep10.obs-nancay.fr@0",os.system,("CleanSHM.py",), {}],
               ["nancep11.obs-nancay.fr@0",os.system,("CleanSHM.py",), {}],
               ]
@@ -126,24 +141,22 @@ def testParallel():
     ListJobs=[["cw10042@0",DDFacet.CleanSHM.driver,(), {}],
               ["cw10042@1",DDFacet.CleanSHM.driver,(), {}],
               ]
-    
+
     callParallel(ListJobs)
-    
+
 def filterHost(jobs):
     site = f"{hostname}@{localrank}"
-    print("FilterHost : %s"%site)
-    #localrank = os.environ.get("SLURM_LOCALID", "0")
+    #print("FilterHost : %s"%site)
     res=[]
     for RunOnHost, func, args, kwargs in jobs:
-        #if RunOnHost == f"{host}@{localrank}":
         if RunOnHost == f"{site}":
-            print("  [exec] [ME=%s][TARGET=%s]: %s(%s,%s)"%(site,RunOnHost,str(func),str(args),str(kwargs)))
+            #print("  [exec] [ME=%s][TARGET=%s]: %s(%s,%s)"%(site,RunOnHost,str(func),str(args),str(kwargs)))
             res.append(func(*args,**kwargs))
         else:
-            print("  [skip] [ME=%s][TARGET=%s]: %s(%s,%s)"%(site,RunOnHost,str(func),str(args),str(kwargs)))
+            #print("  [skip] [ME=%s][TARGET=%s]: %s(%s,%s)"%(site,RunOnHost,str(func),str(args),str(kwargs)))
             pass
     return res
-    
+
 
 def callParallel(ListJobs):
     comm = MPI.COMM_WORLD
@@ -175,22 +188,15 @@ def callParallel(ListJobs):
         return flat
     else:
         return None
-        
-# import itertools
-# a= [7, 8, 4, 5, 9, 10]
-# b= [1, 5, 6]
-# 
-# # Zipping using cycle
-# res= list(zip(a, itertools.cycle(b)))
-# print(res)
+
 class mpi_manager():
     def __init__(self,options_cfg,MSSet, FullMSSet):
         self.options=options_cfg
         self.MSSet=MSSet
         self.FullMSSet=FullMSSet
         # check subset consistency
-        
-        
+
+
         self.ListNodesBeingUsed=FullMSSet.ListNodesBeingUsed if FullMSSet else MSSet.ListNodesBeingUsed
         self.DicoNodes2WorkDir={}
         self.WorkDir=os.getcwd()
@@ -203,7 +209,7 @@ class mpi_manager():
             self.UseMPI=True
 
         # Scatter mslist and big-mslist.txt
-        
+
         self.DoScatterGather=(CheckDistributedFS()==False)
         self.scpScatter(MSSet.file_nodes_mslist)
         if FullMSSet is not None: self.scpScatter(FullMSSet.file_nodes_mslist)
@@ -213,21 +219,21 @@ class mpi_manager():
             LMS=self.FullMSSet.DicoNodes2ListMS[Node]
             for MSName in self.MSSet.DicoNodes2ListMS[Node]:
                 if MSName not in LMS: raise Exception(f"MSs lists not consistent: {MSName} not in {LMS}")
-                
+
 
     def callParallel(self,*args,**kwargs):
         return callParallel(*args,**kwargs)
-    
+
     def givePrefixDDF(self):
         if not self.UseMPI: return ""
-        
+
         if mpirun_singularity:
             return self.givePrefixDDF_mpirun_singularity()
         elif mpirun:
             return "mpirun -n %i "%(self.ddf_nproc)
         elif srun_singularity:
             pass
-                
+
     def createRemoteLocal_mslist(self):
         self.DicoNode2mslist={}
         print(self.MSSet.DicoNodes2ListMS)
@@ -240,7 +246,7 @@ class mpi_manager():
             f.close()
             self.DicoNode2mslist[Node]=FName
             self.scpScatter(FName,Node)
-            
+
     def createRemoteLocal_fullmslist(self):
         self.DicoNode2fullmslist={}
         if self.FullMSSet:
@@ -270,32 +276,30 @@ class mpi_manager():
                 ss="scp -r %s %s:%s"%(FileName,Node,self.WorkDir)
                 print("[Scatter] %s"%ss)
                 os.system("%s > /dev/null 2>&1"%ss)
-                os.system("%s > /dev/null 2>&1"%ss)
         else:
             if NodeDest==self.MainSite: return
-            ss="scp -r %s %s:%s"%(FileName,NodeDest,self.WorkDir)            
+            ss="scp -r %s %s:%s"%(FileName,NodeDest,self.WorkDir)
             print("[Scatter] %s"%ss)
             os.system("%s > /dev/null 2>&1"%ss)
-            #os.system("%s"%ss)
-            
+
     def scpGatherSolutions(self,SolName,DestDir="",NodeSource="all"):
         if not self.UseMPI: return
         if not self.DoScatterGather: return
         if local_rank != 0: return
-        
+
         SolsDir=self.options["SolsDir"]
         AbsSolsDir=os.path.abspath(SolsDir)
-        
+
         for site in self.ListNodesBeingUsed:
             Node=site.split('@')[0]
             lrank=site.split('@')[1]
             if lrank != 0: return
             if Node==self.MainHost:
                 continue
-            
+
             LMS=self.FullMSSet.DicoNodes2ListMS[Node]
-            
-            
+
+
             for MSName in LMS:
                 MSName = Path(MSName).name # if MSName is given with full path
                 os.system("mkdir -p %s/%s"%(SolsDir,MSName))
@@ -303,7 +307,7 @@ class mpi_manager():
                 print("[Gather] %s"%ss)
                 os.system("%s > /dev/null 2>&1"%ss)
                 #os.system("%s"%ss)
-                
+
     def scpScatterSolutions(self,MSName,SmoothSolName,SolsAliasName):
         if not self.UseMPI: return
         if not self.DoScatterGather: return
@@ -311,7 +315,7 @@ class mpi_manager():
         SolsDir=self.options["SolsDir"]
         AbsSolsDir=os.path.abspath(SolsDir)
         Node=self.FullMSSet.DicoMSName2Node[MSName]
-        
+
         MSName = Path(MSName).name # if MSName is given with full path
         os.system("mkdir -p %s/%s"%(SolsDir,MSName))
         if Node!=self.MainHost:
@@ -319,11 +323,11 @@ class mpi_manager():
             print("[Scatter Sols] %s"%ss)
             #os.system("%s &>/dev/null"%ss)
             os.system("%s > /dev/null 2>&1"%ss)
-        
+
             ss="ssh %s rm -f %s"%(Node,SolsAliasName)
             print(ss)
             os.system("%s > /dev/null 2>&1"%ss)
-            
+
             ss="ssh %s ln -s %s %s"%(Node,SmoothSolName,SolsAliasName)
             print(ss)
             os.system("%s > /dev/null 2>&1"%ss)
@@ -331,16 +335,8 @@ class mpi_manager():
             if os.path.islink(SolsAliasName):
                 os.unlink(SolsAliasName)
             os.symlink(SmoothSolName,SolsAliasName)
-                
 
-    # def createLink(self):
-    #     os.symlink(os.path.abspath('%s_%.2f_smoothed.npz'%(ddsols,start_time)),symsolname)
 
-                
-
-def ftest(x):
-    print(x)
-    
 if __name__=="__main__":
     # masterNode = MPI.Get_processor_name()
     testParallel()
